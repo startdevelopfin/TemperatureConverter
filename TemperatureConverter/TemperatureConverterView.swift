c3fcf4a06cb9f5b3380ae33d97f8a54a9877e942
startdevelopfin <danieljaupont@gmail.com>
2025-02-28 18:26:27 -0500

    Create TemperatureConverterView

---
+++ TemperatureConverter/TemperatureConverterView.swift
@@ -0,0 +1,18 @@
+//
+//  TemperatureConverterView.swift
+//  TemperatureConverter
+//
+//  Created by Dan Aupont on 2/28/25.
+//
+
+import SwiftUI
+
+struct TemperatureConverterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemperatureConverterView()
+}
