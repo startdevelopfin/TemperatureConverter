9854dd8580fd44a86b805ee6a393812a8345582f
startdevelopfin <danieljaupont@gmail.com>
2025-02-28 18:36:19 -0500

    Adds state properties for user input and unit selection. Defines an array of temperature units.

--- TemperatureConverter/TemperatureConverterView.swift
+++ TemperatureConverter/TemperatureConverterView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct TemperatureConverterView: View {
+    
+    /// The user's input temperature as a string.
+    @State private var inputTemperature = ""
+    /// The index of the selected input unit.
+    @State private var selectedInputUnit = 0
+    /// The index of the selected output unit.
+    @State private var selectedOutputUnit = 1
+    
+    /// Available temperature units.
+    private let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
