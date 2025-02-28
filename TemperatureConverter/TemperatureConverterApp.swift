7bdb93d0e815fbe1834c54f3a14d07d43bef86fd
startdevelopfin <danieljaupont@gmail.com>
2025-02-28 18:12:54 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Dan Aupont on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TemperatureConverterTests/TemperatureConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterTests.swift
+//  TemperatureConverterTests
+//
+//  Created by Dan Aupont on 2/28/25.
+//
+
+import Testing
+@testable import TemperatureConverter
+
+struct TemperatureConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
